257392830ca72333cefb044b8e9b6ef10cb9aae5
Hercilio M. Ortiz <herciliomartins@gmail.com>
2023-11-01 20:04:01 -0300

    Update: Addition of user in the entries response

--- wall-app-api/entries/serializers.py
+++ wall-app-api/entries/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import Entry
+from ..users.serializers import UserSerializer
 
 
 class EntrySerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Entry
         fields = '__all__'
