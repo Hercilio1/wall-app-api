37f9747509f2b897ee0207f61a62b17ff1f1c6e0
Hercilio M. Ortiz <herciliomartins@gmail.com>
2023-11-04 20:37:33 -0300

    Feat: Welcome email added

--- wall_app_api/users/views.py
+++ wall_app_api/users/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets, mixins, status
+from rest_framework import viewsets, mixins
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from django.core.mail import EmailMessage
+from django.template import loader
 from .models import User
 from .serializers import CreateUserSerializer, UserSerializer
 
@@ -23,3 +25,26 @@ class UserCreateViewSet(mixins.CreateModelMixin,
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+
+        if response.status_code == 201:
+            user = response.data
+            print(user)
+            self.send_welcome_email(user['email'])
+
+        return response
+
+    def send_welcome_email(self, user_email):
+        fromEmail = 'herciliomartins@gmail.com'
+        subject = 'Welcome to Wall App!'
+        template = loader.get_template('email/welcome.html')
+
+        context = {'user_email': user_email}
+        html_content = template.render(context)
+
+        email = EmailMessage(subject, html_content, fromEmail, [user_email])
+        email.content_subtype = 'html'
+
+        email.send()
