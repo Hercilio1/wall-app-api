3de6054a69fcfefb2a1f94bc9bc440a177a0178b
Hercilio M. Ortiz <herciliomartins@gmail.com>
2023-11-08 22:39:20 -0300

    Feat: Addition of Entry content length limit

--- wall_app_api/entries/models.py
+++ wall_app_api/entries/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.conf import settings
-
+from django.core.validators import MaxLengthValidator
 
 class Entry(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # TODO: Make readonly.
-    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(validators=[
+        MaxLengthValidator(limit_value=280, message="Content must be a maximum of 280 characters.")
+    ])
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
