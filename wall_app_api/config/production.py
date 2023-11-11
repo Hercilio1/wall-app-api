1e6c6b2b8302f7e1c9b262a918e68febc9803fa8
Hercilio M. Ortiz <herciliomartins@gmail.com>
2023-11-11 13:00:56 -0300

    SMTP structure update 2

--- wall_app_api/config/production.py
+++ wall_app_api/config/production.py
@@ -12,7 +12,7 @@ class Production(Common):
 
     EMAIL_USE_TLS = True
     EMAIL_USE_SSL = False
-    EMAIL_PORT = os.getenv('EMAIL_PORT', "")
-    EMAIL_HOST = os.getenv('EMAIL_HOST', "")
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', "")
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', "")
+    EMAIL_PORT = os.getenv('EMAIL_PORT', '587')
+    EMAIL_HOST = os.getenv('EMAIL_HOST', '')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
